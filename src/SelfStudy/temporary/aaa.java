297f2e1df71087ed801b59f0801dbc96dda68d30
bbongcat <lollolllo328@gmail.com>
2021-02-17 17:53:03 +0900

    self-study

--- src/SelfStudy/temporary/aaa.java
+++ src/SelfStudy/temporary/aaa.java
@@ -1,13 +1,11 @@
 package SelfStudy.temporary;
 
-public class aaa {
 
+public class aaa {
     public static void main(String[] args) {
 
-        int num1 = 10;
-        double num2 = 2.0;
-        int result = (int)(num1 + num2);
-        System.out.println(result);
+
+
 
     }
 }
